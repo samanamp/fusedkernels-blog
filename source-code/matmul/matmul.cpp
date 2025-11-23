27b275bb7d70bad71d987f2a3058d1618c022930
saman <samanamp@outlook.com>
2025-11-22 21:56:04 -0800

    added matmul

---
+++ source-code/matmul/matmul.cpp
@@ -0,0 +1,65 @@
+#include <ATen/ATen.h>
+#include <c10/cuda/CUDAException.h>
+#include <c10/cuda/CUDAGuard.h>
+#include <cuda_runtime.h>
+#include <torch/extensions.h>
+
+extern "C" void launch_matmul_kernel(float *out, const float *a, const float *b,
+                                     int M, int N, int K, dim3 dimGrid,
+                                     dim3 dimBlock, cudaStream_t stream);
+
+at::Tensor matmul_cuda(at::Tensor a, at::Tensor b)
+{
+    int64_t M = a.size(0), K = a.size(1), N = b.size(1);
+    // Create out tensor placeholder in memory of device
+    auto out = torch::empty({M, N}, a.options());
+
+    // Create pointers to the Address of the tensors on HBM
+    const float *a_ptr = a.data_ptr<float>();
+    const float *b_ptr = b.data_ptr<float>();
+    const float *out_ptr = out.data_ptr<float>();
+
+    // prep stream
+    cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
+    dim3 dimBlock(32, 32);
+    dim3 dimGrid((N + dimBlock.x - 1) / dimBlock.x,
+                 (M + dimBlock.y - 1) / dimBlock.y);
+    launch_matmul_kernel(out_ptr, a_ptr, b_ptr, M, N, K, dimGrid, dimBlock, stream);
+
+    return out;
+}
+
+extern "C" void launch_matmul_kernel_coalescing(float *out, const float4 *a,
+                                                const float4 *b_T, int M, int N,
+                                                int K, dim3 dimGrid, dim3 dimBlock,
+                                                cudaStream_t stream);
+
+at::Tensor matmul_cuda_coalescing(at::Tensor a, at::Tensor b)
+{
+    int64_t M = a.size(0), K = a.size(1), N = b.size(1);
+    TORCH_CHECK(K % 4 == 0, "K must be multiple of 4 when using float4 packing");
+
+    auto out = torch::empty({M, N}, a.options());
+    TORCH_CHECK(reinterpret_cast<uintptr_t>(a.data_ptr<float>()) % 16 == 0,
+                "Tensor A is not 16-byte aligned");
+
+    const float4 *a_ptr = reinterpret_cast<const float4 *>(a.data_ptr<float>());
+    auto bT = b.transpose(0, 1).contiguous();
+    TORCH_CHECK(reinterpret_cast<uintptr_t>(bT.data_ptr<float>()) % 16 == 0,
+                "Tensor B is not 16-byte aligned");
+    const float4 *b_T_ptr = reinterpret_cast<const float4 *>(bT.data_ptr<float>());
+
+    const float *out_ptr = out.data_ptr<float>();
+
+    cudaStream_t stream = c10::cuda::getCurrentCUDAStream();
+    dim3 dimBlock(32, 32);
+    dim3 dimGrid((N + dimBlock.x - 1) / dimBlock.x,
+                 (M + dimBlock.y - 1) / dimBlock.y);
+    launch_matmul_kernel_coalescing(out_ptr, a_ptr, b_T_ptr, M, N, K, dimGrid, dimBlock, stream);
+    return out;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("matmul_cuda", &matmul_cuda_coalescing, "MATMUL (CUDA)");
+}
---
+++ source-code/matmul/matmul_kernel.cu
@@ -0,0 +1,105 @@
+#include <cstdint>
+#include <cuda_runtime.h>
+#include <float.h>
+#include <math.h>
+
+#define TILE_WIDTH 32
+
+__global__ void matmul_kernel_cache(float *out, const float *a, const float *b,
+                                    int M, int N, int K)
+{
+    int row = blockIdx.y * blockWidth.y + threadIdx.y;
+    int col = blockIdx.x * blockWidth.x + threadIdx.x;
+
+    __shared__ float sA[TILE_WIDTH][TILE_WIDTH], sB[TILE_WIDTH][TILE_WIDTH];
+    if (row < M && col < N)
+    {
+        int numTiles = (K + TILE_WIDTH - 1) / TILE_WIDTH;
+        float pVal = 0;
+        for (int t = 0; t < numTiles; t++)
+        {
+            int A_row = row;
+            int A_col = t * TILE_WIDTH + threadIdx.x;
+            if (A_row < M && A_col < K)
+                sA[threadIdx.y][threadIdx.x] = a[A_row * K + A_col];
+            else
+                sA[threadIdx.y][threadIdx.x] = 0;
+
+            int B_row = t * TILE_WIDTH + threadIdx.y;
+            int B_col = col;
+            if (A_row < M && A_col < K)
+                sB[threadIdx.y][threadIdx.x] = b[B_row * N + B_col];
+            else
+                sB[threadIdx.y][threadIdx.x] = 0;
+
+            __syncthreads();
+
+            for (int j = 0; j < TILE_WIDTH; j++)
+                pVal += sA[threadIdx.y][j] * sB[j][threadIdx.x];
+            __syncthreads();
+        }
+        out[row * N + col] = pVal;
+    }
+}
+
+extern "C" void launch_matmul_kernel(float *out, const float *a, const float *b,
+                                     int M, int N, int K, dim3 dimGrid,
+                                     dim3 dimBlock, cudaStream_t stream)
+{
+    int shmem = 0;
+    matmul_kernel_cache<<<dimGrid, dimBlock, shmem, stream>>>(out, a, b, M, N, K);
+}
+
+__global__ void matmul_kernel_cache_coalescing(float *out, const float4 *a, const float4 *b_T,
+                                               int M, int N, int K)
+{
+    int row = blockIdx.y * blockWidth.y + threadIdx.y;
+    int col = blockIdx.x * blockWidth.x + threadIdx.x;
+
+    if (row < M && col < N)
+    {
+        int K4 = (K + 3) / 4;
+
+        int numTiles = (K4 + TILE_WIDTH - 1) / TILE_WIDTH;
+
+        __shared__ float4 sA[TILE_WIDTH][TILE_WIDTH], sB[TILE_WIDTH][TILE_WIDTH];
+        float pVal = 0;
+        for (int t = 0; t < numTiles; t++)
+        {
+            int a_kchunk = t * TILE_WIDTH + threadIdx.x;
+            int b_kchunk = t * TILE_WIDTH + threadIdx.y;
+
+            if (a_kchunk < K4)
+                sA[threadIdx.y][threadIdx.x] = a[row * K4 + a_kchunk];
+            else
+                sA[threadIdx.y][threadIdx.x] = make_float(0.f, 0.f, 0.f, 0.f);
+
+            if (b_kchunk < K4)
+                sB[threadIdx.y][threadIdx.x] = b_T[col * K4 + b_kchunk];
+            else
+                sB[threadIdx.y][threadIdx.x] = make_float(0.f, 0.f, 0.f, 0.f);
+
+            __syncthreads();
+
+            for (int j = 0; j < TILE_WIDTH; j++)
+            {
+                float4 va = sA[threadIdx.y][j];
+                float4 vb = sB[j][threadIdx.x];
+
+                pVal += va.x * vb.x + va.y * vb.y + va.z * vb.z + va.w * vb.w;
+            }
+            __syncthreads();
+        }
+        out[row * N + col] = pVal;
+    }
+}
+
+extern "C" void launch_matmul_kernel_coalescing(float *out, const float4 *a,
+                                                const float4 *b_T, int M, int N,
+                                                int K, dim3 dimGrid, dim3 dimBlock,
+                                                cudaStream_t stream)
+{
+    int shmem = 0;
+    matmul_kernel_cache_coalescing<<<dimGrid, dimBlock, shmem, stream>>>(
+        out, a, b_T, M, N, K);
+}
